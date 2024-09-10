32b6950b8479cdc598b27580300b4e3eea4b265f
JoseMartinezM <josemtzmor2013@hotmail.com>
2024-09-10 07:48:54 -0600

    Añadi color morado y Dibujar circulo

--- paint.py
+++ paint.py
@@ -36,9 +36,17 @@ def square(start, end):
     end_fill()
 
 
-def circle(start, end):
+def circle(start, end):     #Añadi la funcion para dibujar el circulo
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    radius = ((end.x - start.x) ** 2 + (end.y - start.y) ** 2) ** 0.5  # Calcula el radio
+    circle(radius)  # Dibuja el círculo con el radio calculado
+
+    end_fill()
 
 
 def rectangle(start, end):
@@ -79,6 +87,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('purple'), 'P')  # Nuevo color
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
